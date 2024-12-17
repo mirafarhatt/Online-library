eb81a1cec2e377c168f057e405e0417425141401
mirafarhatt <162834678+mirafarhatt@users.noreply.github.com>
2024-12-17 19:13:17 +0200

    first commit

---
+++ js/html/footer.js
@@ -0,0 +1,27 @@
+document.write(`
+    <footer > <div class="footer">
+            <div class="container">
+               <div class="row pdn-top-30">
+                  <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+                     <div class="Follow">
+                        <h3>Follow Us</h3>
+                     </div>
+                     <ul class="location_icon">
+                        <li> <a href="https://www.facebook.com/"><img src="icon/facebook.png"></a></li>
+                        <li> <a href="https://www.twitter.com/"><img src="icon/Twitter.png"></a></li>
+                        <li> <a href="https://www.linkedin.com/"><img src="icon/linkedin.png"></a></li>
+                        <li> <a href="https://www.instagram.com/"><img src="icon/instagram.png"></a></li>
+                     </ul>
+                  </div>
+                  <div class="col-xl-8 col-lg-8 col-md-8 col-sm-12">
+                     <div class="Follow">
+                        <h3>To get our offers</h3>
+                     </div>
+                     <input class="Newsletter" placeholder="Enter your email" type="Enter your email">
+                     <button class="Subscribe">Subscribe</button>
+                  </div>
+               </div>
+            </div>
+         </div>
+        </footer>
+         `);
---
+++ js/html/header.js
@@ -0,0 +1,35 @@
+document.write(`
+   <header class="header">
+      <div class="container">
+         <div class="row">
+            <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
+               <div class="full">
+                  <div class="center-desk">
+                     <div class="logo" style="margin-top:-40px;margin-bottom: -30px;"> 
+                        <a href="Home.html"><img src="images/logo1.png" alt="#" width="150px"></a> 
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
+               <div class="menu-area">
+                  <div class="limit-box">
+                     <nav class="main-menu">
+                        <ul class="menu-area-main">
+                           <li><a href="Home.html">Home</a></li>
+                           
+                           <li><a href="library.html">Our Books</a></li>
+                           <li><a href="services.html">Our Services</a></li>
+                           <li><a href="about.html">About us</a></li>
+                           <li><a href="contact.html">Contact us</a></li>
+                           <li class="mean-last"><a href="cart.html"><img src="images/cart.png" alt="#"></a></li>
+                           <li class="mean-last"><a href="sign in.html"><img src="images/top-icon.png" alt="#"></a></li>
+                        </ul>
+                     </nav>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </header>
+`);
